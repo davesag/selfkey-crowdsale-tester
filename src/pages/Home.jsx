45312ef5e840cbd70aeaf5d2a30e7fae9bdba85b
Dave Sag <davesag@gmail.com>
2017-12-28 16:28:55 +1030

    KEY-44 extracted common code into a shared component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 
-import { Row, Col } from 'react-bootstrap'
-
-import Page from '../components/Page.jsx'
-import Title from '../components/Title.jsx'
-import ABI from '../components/Contract/ABI.jsx'
-import OwnerData from '../components/Owner/OwnerData.jsx'
+import PageWithCrowdsale from '../components/PageWithCrowdsale.jsx'
 import Owner from '../components/Owner/Owner.jsx'
 import CrowdsaleData from '../components/Crowdsale/CrowdsaleData.jsx'
 import CrowdsaleDates from '../components/Crowdsale/CrowdsaleDates.jsx'
@@ -16,59 +11,31 @@ import AddPrecommitment from '../components/Precommitment/AddPrecommitment.jsx'
 import BulkAddPrecommitments from '../components/Precommitments/BulkAddPrecommitments.jsx'
 
 const Home = () => (
-  <Page>
-    {() => (
-      <section id="home">
-        <Title title="Selfkey Crowdsale: Home" />
-        <Row className="dashboard">
-          <Col xs={12}>
-            <Row>
-              <Col xs={12}>
-                <h2>Selfkey Test Crowdsale</h2>
-                <ABI contracts={['SelfkeyCrowdsale', 'SelfkeyToken']}>
-                  {({ abis }) => {
-                    const { SelfkeyCrowdsale } = abis
-                    console.debug('SelfkeyCrowdsale', SelfkeyCrowdsale)
-                    if (!SelfkeyCrowdsale) return null
-                    return (
-                      <OwnerData abi={SelfkeyCrowdsale}>
-                        {({ isOwner }) => (
-                          <CrowdsaleData abi={SelfkeyCrowdsale}>
-                            {({ startTime, endTime, tokensPurchased }) => (
-                              <section>
-                                <Owner isOwner={isOwner} />
-                                <CrowdsaleDates
-                                  startDate={startTime}
-                                  endDate={endTime}
-                                />
-                                <CrowdsaleStats
-                                  tokensPurchased={tokensPurchased}
-                                />
-                                <StatusCheck abi={SelfkeyCrowdsale} />
-                                {isOwner && (
-                                  <section>
-                                    <KYCVerification abi={SelfkeyCrowdsale} />
-                                    <AddPrecommitment abi={SelfkeyCrowdsale} />
-                                    <BulkAddPrecommitments
-                                      abi={SelfkeyCrowdsale}
-                                    />
-                                  </section>
-                                )}
-                              </section>
-                            )}
-                          </CrowdsaleData>
-                        )}
-                      </OwnerData>
-                    )
-                  }}
-                </ABI>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </section>
+  <PageWithCrowdsale
+    slug="home"
+    title="Selfkey Crowdsale: Home"
+    heading="Selfkey Test Crowdsale"
+  >
+    {({ SelfkeyCrowdsale, isOwner }) => (
+      <CrowdsaleData abi={SelfkeyCrowdsale}>
+        {({ startTime, endTime, tokensPurchased }) => (
+          <section>
+            <Owner isOwner={isOwner} />
+            <CrowdsaleDates startDate={startTime} endDate={endTime} />
+            <CrowdsaleStats tokensPurchased={tokensPurchased} />
+            <StatusCheck abi={SelfkeyCrowdsale} />
+            {isOwner && (
+              <section>
+                <KYCVerification abi={SelfkeyCrowdsale} />
+                <AddPrecommitment abi={SelfkeyCrowdsale} />
+                <BulkAddPrecommitments abi={SelfkeyCrowdsale} />
+              </section>
+            )}
+          </section>
+        )}
+      </CrowdsaleData>
     )}
-  </Page>
+  </PageWithCrowdsale>
 )
 
 export default Home
