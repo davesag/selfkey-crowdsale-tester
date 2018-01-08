383623d121265cafcb9dc0ec2f0f4050cbf74b98
Dave Sag <davesag@gmail.com>
2018-01-08 12:25:52 +1100

    n/a less functional, more proceedural, the for loop proves useful

--- src/utils/seriesOfPromises.js
+++
@@ -1,8 +0,0 @@
-const seriesOfPromises = arrayOfPromises =>
-  arrayOfPromises.reduce(
-    (acc, elem) =>
-      acc.then(result => elem.then(Array.prototype.concat.bind(result))),
-    Promise.resolve([])
-  )
-
-export default seriesOfPromises
