250a39567944b97283d61e464c2619d11a765972
Dave Sag <davesag@gmail.com>
2018-01-09 13:36:07 +1100

    KEY-66 added ability to bulk verify addresses

--- src/pages/KYC.jsx
+++ src/pages/KYC.jsx
@@ -4,6 +4,7 @@ import PageWithOwner from '../components/PageWithOwner.jsx'
 import Owner from '../components/Owner/Owner.jsx'
 import StatusCheck from '../components/StatusCheck/StatusCheck.jsx'
 import KYCVerification from '../components/KYC/KYCVerification.jsx'
+import BulkVerifyKYC from '../components/BulkKYC/BulkVerifyKYC.jsx'
 
 const KYC = () => (
   <PageWithOwner slug="kyc" title="Selfkey Crowdsale: KYC" heading="KYC">
@@ -11,7 +12,12 @@ const KYC = () => (
       <section>
         <Owner isOwner={isOwner} />
         <StatusCheck />
-        {isOwner && <KYCVerification />}
+        {isOwner && (
+          <section id="kyc-verification">
+            <KYCVerification />
+            <BulkVerifyKYC />
+          </section>
+        )}
       </section>
     )}
   </PageWithOwner>
